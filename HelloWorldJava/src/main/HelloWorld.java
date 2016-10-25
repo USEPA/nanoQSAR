b0a3e92117f69a3e11b5046f7b23727f04e50d8d
Paul Harten <harten.paul@epa.gov>
2016-10-25 15:33:59 -0400

    Try HelloWorld Project in Java

---
+++ HelloWorldJava/src/main/HelloWorld.java
@@ -0,0 +1,15 @@
+package main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			System.out.println("HelloWorld in Java\n");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
