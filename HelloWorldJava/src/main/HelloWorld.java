8a8a6b0a31a68b1095bd0fa48aff8001de27b925
Paul Harten <harten.paul@epa.gov>
2016-10-27 09:26:41 -0400

    Modified HelloWorldJava in the develop branch.

--- HelloWorldJava/src/main/HelloWorld.java
+++ HelloWorldJava/src/main/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			System.out.println("HelloWorld in Java\n");
+			System.out.println("HelloWorld in Java, again\n");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
