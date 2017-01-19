fae7b21f5e7f10d9e6c46b297009e51ff138d059
wilmelz <melendez.wilson@epa.gov>
2017-01-18 19:03:53 -0600

    Added AllPLSRTests.java.

---
+++ nanoQsarMatrixBuilder/src/test/java/AllPLSRTests.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ CsvMatrixTest.class, MatrixTest.class })
+public class AllPLSRTests {
+
+}
