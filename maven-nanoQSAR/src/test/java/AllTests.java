6d5e33fb22820efac4052f05f9d725a6c4f40ce6
wilmelz <melendez.wilson@epa.gov>
2017-05-22 16:01:43 -0500

    Added JUnit test.

--- maven-nanoQSAR/src/test/java/AllTests.java
+++ maven-nanoQSAR/src/test/java/AllTests.java
@@ -3,9 +3,10 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ConnectionManagerTest.class, CsvFileWriterTest.class,
-		DefaultUnitsTest.class, IllegalUnitsExceptionTest.class,
-		MySqlQueryTest.class, NanoMaterialTest.class })
+@SuiteClasses({ConnectionManagerTest.class,
+		CsvFileWriterTest.class, DefaultUnitsTest.class,
+		IllegalUnitsExceptionTest.class, MySqlQueryTest.class,
+		NanoMaterialTest.class })
 public class AllTests {
 
 }
--- maven-nanoQSAR/src/test/java/NanoMaterialTest.java
+++ maven-nanoQSAR/src/test/java/NanoMaterialTest.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.io.File;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -106,6 +109,28 @@ public class NanoMaterialTest {
 		String[] actualCsvHeader = NanoMaterial.getHeaderFile();  // Actual header 
 		
 		Assert.assertArrayEquals(expectedCsvHeader, actualCsvHeader);
+	}
+	
+	/**
+	 * This test performs the following steps:
+	 * 1) it runs the whole application.
+	 * 2) it checks whether the CSV file was created.
+	 * 3) it checks whether the CSV file is empty or not.
+	 * @author Wilson Melendez
+	 */
+	@Test
+	public void testMainProgram()
+	{
+		String[] args = null;
+		/* Run the application. */
+		Main.main(args);
+		
+		/* Verify that the CSV file was created and that it's not
+		 * empty. */
+		String csvFile = System.getProperty("user.dir") + "\\nanoQSAR.csv";		
+		File file = new File(csvFile);
+		assertTrue("CSV file exists.", file.exists());
+		assertTrue("CSV file is not empty.", file.length() > 0);		
 	}
 
 }
