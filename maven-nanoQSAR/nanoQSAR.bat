dc722118313c3c51b3381019ea34a88823629445
Paul Harten <harten.paul@epa.gov>
2017-08-07 09:37:46 -0400

    nanoQsar.bat file to execute nanoQSAR.jar and nanoQSAR_PLS.jar consecutively.

---
+++ maven-nanoQSAR/nanoQSAR.bat
@@ -0,0 +1,19 @@
+:: This script runs the nanoQSAR application using 3 jar files.
+
+@echo off
+
+echo Beginning execution of batch file.
+
+echo Running batch script from %~dp0
+cd %~dp0
+
+echo Beginning execution of the data mining application.
+java -jar nanoQSAR.jar
+echo Ending execution of the data mining application.
+
+echo Beginning execution of the PLS Regression algorithm.
+java -jar nanoQSAR_PLS.jar
+echo Ending execution of the PLS Regression application.
+
+echo Ending execution of batch file.
+
