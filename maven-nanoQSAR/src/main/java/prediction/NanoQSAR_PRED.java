85da9866a54d86d2e52524d0adaa9fdf28972d26
Paul Harten <harten.paul@epa.gov>
2018-01-25 15:40:57 -0500

    Change NanoQSAR, NanoQSAR_PRED and testers to use constructors not main.

--- maven-nanoQSAR/src/main/java/prediction/NanoQSAR_PRED.java
+++ maven-nanoQSAR/src/main/java/prediction/NanoQSAR_PRED.java
@@ -4,6 +4,7 @@
 package prediction;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -32,113 +33,152 @@ public class NanoQSAR_PRED {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public NanoQSAR_PRED(String[] args) {
 		
 		try	
 		{	
-			if (args == null || args.length == 0) { // Use default properties file.
+			if (testArgs(args)) {
 
+				startLogger();
 
-			} else if (args.length == 1 && args[0].trim().matches("-h")) { // respond with user options
+				predict();
 
-				System.out.print(helpString);
-				return;
-
-			} else if (args.length == 1) { // Using command-line entered properties file
-
-				propFilename = args[0].trim();
-
-			} else { // Something is wrong
-
-				throw new Exception("Invalid options are used");
-				
 			}
-
-		    /* Initialize log file information. Throw IOException and/or SecurityException 
-		     * if creation of file handler was not successful. */
-			LOGGER1.addHandler(new FileHandler(logFilename));
-			LOGGER1.setLevel(Level.INFO);
-			LOGGER1.setUseParentHandlers(false);  // This will prevent LOGGER from printing messages to the console.
-		
-		    /* Input filenames stored in properties file. */
-			DBUtil.loadProperties(propFilename);	
-			
-		    /* Make a copy of nanoQSAR.csv. */
-			String originalFilename = DBUtil.getCsvFileName();
-			String testFilename = DBUtil.getPredictionsFileName();
-		    File source = new File(originalFilename);
-		    File dest = new File(testFilename);
-		
-		    Predictor predictor = new Predictor();	
-		    predictor.copyFiles(source, dest);		
-		
-			/* Read CSV file with data that had been mined from database. */
-			NanoToxExps nanoToxExps = new NanoToxExps(testFilename);			
-			
-			/* Select the columns that will be used to build X and Y matrices from nanoToxExps. */
-			nanoToxExps.selectContinuousColumns();
-			nanoToxExps.selectResultColumns();
-			
-			/* Build Matrices from experimental data. */
-			CsvMatrix csvm = new CsvMatrix();
-			csvm.buildMatrices(nanoToxExps);
-			
-			/* Get a copy of the X matrix and replace null entries with averages or zeroes. */			
-			DoubleMatrix xmatrix = CsvMatrix.getxMatrix();
-			predictor.setTestXmatrix(xmatrix);
-			predictor.replaceNullsXmatrix();
-			
-			/* Read the beta coefficients from CSV file and store them in a matrix. */
-			int xcol = xmatrix.columns;
-			int ycol = CsvMatrix.getyMatrix().columns;
-			String betaFilename = DBUtil.getBplsFileName();
-			predictor.readCsvFile(betaFilename, xcol, ycol);
-			
-			/* Get header of X-matrix */
-			String[] xMatrixHeader = nanoToxExps.getDescriptorHeader();
-		
-			/* Get header of Y-Matrix */
-			String[] yMatrixHeadr = nanoToxExps.getResultHeader();
-			
-			/* Get header of BPLS matrix */
-			String[] bplsHeader = predictor.getHeader();			
-			
-			/* Verify that the headers of the X and Beta-Coefficients matrices match. */
-			/* If a mismatch in the headers is found, an exception will be thrown. */
-			predictor.verifyOrderHeaders(xMatrixHeader, bplsHeader);
-			
-			/* Get a copy of the beta coefficients matrix. */
-			DoubleMatrix bMatrix = predictor.getBetaMatrix();			
-			
-			/* Get a copy of the processed X matrix (with replaced nulls).  */
-			DoubleMatrix testXmatrix = predictor.getTestXmatrix();
-			
-			/* Predict results using the X matrix and beta coefficients. */
-			DoubleMatrix yPred = CsvMatrix.predictResults(testXmatrix, bMatrix);
-			
-			/* Build test header  */
-			predictor.buildTestHeader(xMatrixHeader, yMatrixHeadr);
-			
-			/* Get copy of the original Y matrix  */
-			DoubleMatrix yMatrix = CsvMatrix.getyMatrix();
-		
-			/* Calculate R2: use the original Y matrix and predicted Y matrix. */
-			double[] r2 = predictor.calculateR2(yMatrix, yPred);
-			
-			/* Store R2 in the logger file. */
-			LOGGER1.info("R2[0] = " + r2[0]);
-			
-			/* Build results matrix that will be written to output. */
-			predictor.buildResultsMatrix(testXmatrix, yMatrix, yPred);
-			
-			/* Write test results to CSV file. */
-			predictor.writeResultsToCsv(testFilename);
 		}
 		catch (Exception ex) 
 		{
 			ex.printStackTrace();
 		}
 				
+	}
+	
+	public NanoQSAR_PRED() {
+		
+		try	
+		{	
+
+			startLogger();
+
+			predict();
+
+		}
+		catch (Exception ex) 
+		{
+			ex.printStackTrace();
+		}		
+		
+	}
+
+	private boolean testArgs(String[] args) throws Exception {
+		
+		if (args == null || args.length == 0) { // Use default properties file.
+
+
+		} else if (args.length == 1 && args[0].trim().matches("-h")) { // respond with user options
+
+			System.out.print(helpString);
+			return false;
+
+		} else if (args.length == 1) { // Using command-line entered properties file
+
+			propFilename = args[0].trim();
+
+		} else { // Something is wrong
+
+			throw new Exception("Invalid options are used");
+			
+		}
+		
+		return true;
+		
+	}
+	
+	private void startLogger() throws IOException {
+		
+		/* Initialize log file information. Throw IOException and/or SecurityException 
+		 * if creation of file handler was not successful. */
+		LOGGER1.addHandler(new FileHandler(logFilename));
+		LOGGER1.setLevel(Level.INFO);
+		LOGGER1.setUseParentHandlers(false);  // This will prevent LOGGER from printing messages to the console.
+		
+	}
+	
+	private void predict() throws Exception {
+		
+		/* Input filenames stored in properties file. */
+		DBUtil dbUtil = new DBUtil();
+		dbUtil.loadProperties(propFilename);	
+
+		/* Make a copy of nanoQSAR.csv. */
+		String originalFilename = dbUtil.getCsvFileName();
+		String testFilename = dbUtil.getPredictionsFileName();
+		File source = new File(originalFilename);
+		File dest = new File(testFilename);
+
+		Predictor predictor = new Predictor();	
+		predictor.copyFiles(source, dest);		
+
+		/* Read CSV file with data that had been mined from database. */
+		NanoToxExps nanoToxExps = new NanoToxExps(testFilename);			
+
+		/* Select the columns that will be used to build X and Y matrices from nanoToxExps. */
+		nanoToxExps.selectContinuousColumns();
+		nanoToxExps.selectResultColumns();
+
+		/* Build Matrices from experimental data. */
+		CsvMatrix csvm = new CsvMatrix();
+		csvm.buildMatrices(nanoToxExps);
+
+		/* Get a copy of the X matrix and replace null entries with averages or zeroes. */			
+		DoubleMatrix xmatrix = csvm.getxMatrix();
+		predictor.setTestXmatrix(xmatrix);
+		predictor.replaceNullsXmatrix();
+
+		/* Read the beta coefficients from CSV file and store them in a matrix. */
+		int xcol = xmatrix.columns;
+		int ycol = csvm.getyMatrix().columns;
+		String betaFilename = dbUtil.getBplsFileName();
+		predictor.readCsvFile(betaFilename, xcol, ycol);
+
+		/* Get header of X-matrix */
+		String[] xMatrixHeader = nanoToxExps.getDescriptorHeader();
+
+		/* Get header of Y-Matrix */
+		String[] yMatrixHeadr = nanoToxExps.getResultHeader();
+
+		/* Get header of BPLS matrix */
+		String[] bplsHeader = predictor.getHeader();			
+
+		/* Verify that the headers of the X and Beta-Coefficients matrices match. */
+		/* If a mismatch in the headers is found, an exception will be thrown. */
+		predictor.verifyOrderHeaders(xMatrixHeader, bplsHeader);
+
+		/* Get a copy of the beta coefficients matrix. */
+		DoubleMatrix bMatrix = predictor.getBetaMatrix();			
+
+		/* Get a copy of the processed X matrix (with replaced nulls).  */
+		DoubleMatrix testXmatrix = predictor.getTestXmatrix();
+
+		/* Predict results using the X matrix and beta coefficients. */
+		DoubleMatrix yPred = CsvMatrix.predictResults(testXmatrix, bMatrix);
+
+		/* Build test header  */
+		predictor.buildTestHeader(xMatrixHeader, yMatrixHeadr);
+
+		/* Get copy of the original Y matrix  */
+		DoubleMatrix yMatrix = csvm.getyMatrix();
+
+		/* Calculate R2: use the original Y matrix and predicted Y matrix. */
+		double[] r2 = predictor.calculateR2(yMatrix, yPred);
+
+		/* Store R2 in the logger file. */
+		LOGGER1.info("R2[0] = " + r2[0]);
+
+		/* Build results matrix that will be written to output. */
+		predictor.buildResultsMatrix(testXmatrix, yMatrix, yPred);
+
+		/* Write test results to CSV file. */
+		predictor.writeResultsToCsv(testFilename);
 		
 	}
 
