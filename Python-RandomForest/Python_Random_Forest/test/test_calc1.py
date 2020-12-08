b3659533e134f95a9fc3b08a754b772d5d5662eb
wmelende <melendez.wilson@epa.gov>
2020-12-08 00:27:34 -0600

    Added pytest version of test_calc.py.

---
+++ Python-RandomForest/Python_Random_Forest/test/test_calc1.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 23, 2020
+
+@author: pharten
+'''
+
+from Calc import add, subtract
+
+def test_add():
+    assert add(10, 5) == 15
+    assert add(-1, -1) == -2
+    assert add(-1, 1) == 0
+       
+        
+def test_subtract():
+    assert subtract(10, 5) == 5
+    assert subtract(-1, 1) == -2
+    assert subtract(-1,- 1) == 0
