8a6e435aed1d40db68d3d9e26e13fcaf36e51dd4
wmelende <melendez.wilson@epa.gov>
2020-12-31 14:09:23 -0600

    Added two missing modules and deleted large CSV file.

---
+++ Python-RandomForest/Python_Random_Forest/src/Replace_Null_with_None.py
@@ -0,0 +1,32 @@
+'''
+This module contains a function that replaces NULL entries with Python's None.
+
+Created on Dec 21, 2020
+
+@author: Wmelende
+'''
+
+def replace_null_with_none(df):
+    '''
+    Name
+    ----
+    replace_null_with_none
+    
+    Description
+    -----------
+    This function replaces NULL entries with Python's None object.
+    
+    Input Parameters
+    ----------------
+    df : DataFrame
+        DataFrame containing the in vitro rows.
+    
+    Output Parameters
+    -----------------
+    Modified DataFrame df.
+    
+    '''
+    # Replace all NULL entries with Python's object.
+    df = df.replace({'NULL': None})
+    
+    return df
