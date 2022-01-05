ba0adf5dd58739a8cae0c252eb85518767f4539c
Paul Harten <Harten.Paul@epa.gov>
2022-01-05 12:30:27 -0500

    Process all contaminant unit transformations in loop.

--- Python-RandomForest/Python_Random_Forest/src/Process_Contaminants_Units.py
+++ Python-RandomForest/Python_Random_Forest/src/Process_Contaminants_Units.py
@@ -4,7 +4,7 @@ processes the units for the contaminants.
 
 Created on Feb 15, 2021
 
-@author: Wmelende
+@author: Wmelende, Pharten
 '''
 import math
  
@@ -34,16 +34,16 @@ def process_contaminants_units(df):
     '''   
     # Determine number of rows in data frame.
     nrow = len(df.index)
-    
+    column_names = list(df.columns)
+    subs_value = "_contaminant_value"
+    contam_columns  = [icol for icol in column_names if subs_value in icol]
+    ''' Double Check this works, use similar conversions for additives '''
     # ########################################################################################################
     # Process contaminant units
     # ########################################################################################################
-    
-    # Process cobalt units
-    col_value = "cobalt contaminant_value"
-    col_units = "cobalt contaminant_unit"
-    if col_value in df.columns:
+    for col_value in list(contam_columns):
         df[col_value] = df[df[col_value]!= None][col_value].astype(float)
+        col_units = col_value.replace("_value","_unit")
         try:
             for irow in range(0, nrow):
                 if (math.isnan(df[col_value].iloc[irow])):
@@ -53,81 +53,7 @@ def process_contaminants_units(df):
                     if (str_units == 'parts per million' or 
                         str_units == 'parts per billion'):
                         if (str_units == 'parts per billion'):
-                            df.loc[irow, col_units] = 'parts per million' 
-                            df.loc[irow, col_value] = df.loc[irow, col_value] * 1.0E-03
-                    else:
-                        error_message = ("Unrecognized units: " + str_units + ", column = " + 
-                                         str(col_value) + " at row " + str(irow))
-                        raise ValueError(error_message) 
-                                           
-        except ValueError as msg:
-            error_message = str(msg) + ", " + str(col_value) + ", " + col_units +  ", row = " + str(irow)
-            print(error_message)  
-        
-    # Process potassium units
-    col_value = "potassium contaminant_value"
-    col_units = "potassium contaminant_unit"
-    if col_value in df.columns:
-        df[col_value] = df[df[col_value]!= None][col_value].astype(float)
-        try:
-            for irow in range(0, nrow):
-                if (math.isnan(df[col_value].iloc[irow])):
-                    continue
-                else:
-                    str_units = df[col_units].iloc[irow]
-                    if (str_units == 'parts per million' or 
-                        str_units == 'parts per billion'):
-                        if (str_units == 'parts per billion'):
-                            df.loc[irow, col_units] = 'parts per million' 
-                            df.loc[irow, col_value] = df.loc[irow, col_value] * 1.0E-03
-                    else:
-                        error_message = ("Unrecognized units: " + str_units + ", column = " + 
-                                         str(col_value) + " at row " + str(irow))
-                        raise ValueError(error_message) 
-                                           
-        except ValueError as msg:
-            error_message = str(msg) + ", " + str(col_value) + ", " + col_units +  ", row = " + str(irow)
-            print(error_message)  
-    
-    # Process silicon dioxide units
-    col_value = "silicon dioxide contaminant_value"
-    col_units = "silicon dioxide contaminant_unit"
-    if col_value in df.columns:
-        df[col_value] = df[df[col_value]!= None][col_value].astype(float)
-        try:
-            for irow in range(0, nrow):
-                if (math.isnan(df[col_value].iloc[irow])):
-                    continue
-                else:
-                    str_units = df[col_units].iloc[irow]
-                    if (str_units == 'parts per million' or 
-                        str_units == 'parts per billion'):
-                        if (str_units == 'parts per billion'):
-                            df.loc[irow, col_units] = 'parts per million' 
-                            df.loc[irow, col_value] = df.loc[irow, col_value] * 1.0E-03
-                    else:
-                        error_message = ("Unrecognized units: " + str_units + ", column = " + 
-                                         str(col_value) + " at row " + str(irow))
-                        raise ValueError(error_message) 
-                                           
-        except ValueError as msg:
-            error_message = str(msg) + ", " + str(col_value) + ", " + col_units +  ", row = " + str(irow)
-            print(error_message)  
-        
-    # Process vanadium units
-    col_value = "vanadium contaminant_value"
-    col_units = "vanadium contaminant_unit"
-    if col_value in df.columns:
-        df[col_value] = df[df[col_value]!= None][col_value].astype(float)
-        try:
-            for irow in range(0, nrow):
-                if (math.isnan(df[col_value].iloc[irow])):
-                    continue
-                else:
-                    str_units = df[col_units].iloc[irow]
-                    if (str_units == 'parts per million' or 
-                        str_units == 'parts per billion'):
-                        if (str_units == 'parts per billion'):
+                            # convert contaminants to 'parts per million'
                             df.loc[irow, col_units] = 'parts per million' 
                             df.loc[irow, col_value] = df.loc[irow, col_value] * 1.0E-03
                     else:
@@ -138,5 +64,5 @@ def process_contaminants_units(df):
         except ValueError as msg:
             error_message = str(msg) + ", " + str(col_value) + ", " + col_units +  ", row = " + str(irow)
             print(error_message)
-            
+
     return df
