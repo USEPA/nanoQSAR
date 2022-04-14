e76cfb26915a676f60adbc8d86459692da739720
Paul Harten <Harten.Paul@epa.gov>
2022-04-14 15:26:16 -0400

    Add Rsq and Qsq calculations

--- Python-RandomForest/Python_Random_Forest/src/RandomForestAnalysis.py
+++ Python-RandomForest/Python_Random_Forest/src/RandomForestAnalysis.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 from numpy.core.numeric import indices
+#from numpy.array_api._dtypes import float64
 
 class RandomForestAnalysis():
     
@@ -16,22 +17,45 @@ class RandomForestAnalysis():
     def train(self, X_train, y_train):
         self.X_train = X_train
         self.y_train = y_train
-        self.regressor = RandomForestRegressor(n_estimators = 100, random_state = 0)
+        self.regressor = RandomForestRegressor(n_estimators = 300, n_jobs=4, random_state = 0, min_samples_split=4, max_features=0.25)
         self.regressor.fit(self.X_train, self.y_train)
         return self.regressor
     
     def predict(self, X_test):
         return self.regressor.predict(X_test)
     
+    def Rsq(self, X, y):
+        # r2 value = 1 - u/v where u = (((y_train(i)-y_pred(i))**2).sum(i))/n_train
+        # and v = (((y_train(i)-y_train(i).avg())**2).sum(i))/n_train
+        # this the same self.regressor.score(X,y)
+        y_pred = self.regressor.predict(X)
+        y_avg = y.mean()
+        u = ((y-y_pred)**2).sum() / y.size
+        v = ((y-y_avg)**2).sum() / y.size
+        return 1.0-u/v
+    
+    def Qsq(self, X, y):
+        # q2 value = 1 - u/v where u = (((y(i)-y_pred(i))**2).sum(i))/y.count()
+        # and v = (((y_train(i)-y_train(i).avg()).sum(i))/n_train
+        # notice v is based upon self.y_train information, different from what has been used before
+        y_pred = self.regressor.predict(X)
+        y_avg = self.y_train.mean()
+        u = ((y-y_pred)**2).sum() / y.size
+        v = ((self.y_train-y_avg)**2).sum() / self.y_train.size
+        return 1.0-u/v
+    
     # Evaluate the algorithm
-    def evaluate(self, y_test, X_test):
+    def evaluate(self, X_test, y_test):
         y_pred = self.predict(X_test)
         mean_abs_error = metrics.mean_absolute_error(y_test, y_pred)
         mean_sqr_error = metrics.mean_squared_error(y_test, y_pred)
         root_mean_sqr_error = np.sqrt(metrics.mean_squared_error(y_test, y_pred))
     
-        print('Rsq for Training set = ',self.regressor.score(self.X_train, self.y_train))
-        print('Rsq for Testing set: ',self.regressor.score(X_test, y_test))
+        print('Rsq for Training set = ',self.Rsq(self.X_train, self.y_train))
+        print('Score for Training set = ',self.regressor.score(self.X_train, self.y_train))
+        print('Rsq for Testing set: ',self.Rsq(X_test, y_test))
+        print('Score for Testing set: ',self.regressor.score(X_test, y_test))
+        print('Qsq for Testing set: ',self.Qsq(X_test, y_test))
         print('Mean absolute error: ', mean_abs_error)
         print('Mean square error: ', mean_sqr_error)
         print('Root mean square error: ', root_mean_sqr_error)
